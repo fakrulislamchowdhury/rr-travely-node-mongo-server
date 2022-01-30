931e5d2d59162b4cf9f7785c062a4bb3dee5c7f4
fakrul01 <fislamc6@gmail.com>
2022-01-30 08:32:04 +0600

    first commit

---
+++ collection.constant.js
@@ -0,0 +1,6 @@
+// collections
+exports.SERVICE_COLLECTION = "services";
+
+
+// databases 
+exports.RR_TRAVELY_DB = "RR_Travely";
---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const { RR_TRAVELY_DB, SERVICE_COLLECTION } = require("./collection.constant")
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+const cors = require('cors');
+const { get } = require('express/lib/response');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 7000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a29dq.mongodb.net/RR_Travely?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db(RR_TRAVELY_DB);
+        const servicesCollection = database.collection(SERVICE_COLLECTION);
+
+        console.log(database, servicesCollection)
+        console.log(uri);
+
+        // Get API
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+        //Get Single Service
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.json(service);
+        })
+
+        // POST API
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            const result = await servicesCollection.insertOne(service);
+            console.log(result);
+            res.json(result);
+        })
+
+    }
+
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Running RR Travely');
+});
+
+app.listen(port, () => {
+    console.log('Running Server on port', port);
+});
